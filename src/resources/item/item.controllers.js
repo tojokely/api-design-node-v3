1d773afd59aab2ca415ea4ff967ce2fa65133185
tojokely <tojosoaramarlina@gmail.com>
2019-08-26 03:08:42 +0400

    completed

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,1 +1,11 @@
-export default {}
+import { crudControllers } from '../../utils/crud'
+import { Item } from './item.model'
+
+export default crudControllers(Item)
+
+/** overriding crud Controller
+ * ...crudControllers(Item), //spread it
+ * getOne() {
+   what you want get one to be changed to
+ }
+**/
--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,12 +1,50 @@
-export const getOne = model => async (req, res) => {}
+export const getOne = model => async (req, res) => {
+  const id = req.params.id
+  const user = req.user._id
+  const doc = await model.findOne({_id: id, createdBy: userId}).exec()
+  if(!doc) {
+    res.status(404).end()
+  }
+  res.status(200).json({data: doc})
+}
 
-export const getMany = model => async (req, res) => {}
+export const getMany = model => async (req, res) => {
+  const docs = model.find({createdBy: req.user.id}).exec()
+  res.status(200).json({data: docs})
+}
 
-export const createOne = model => async (req, res) => {}
+export const createOne = model => async (req, res) => {
+  // Shallow-cloning using ...
+  // Content of doc is {req.body}
+  const doc =  await model.create({...req.body, createdBy: req.user.id}).exec()
+  const user = req.user._id
+  res.status(201).json({data: docs})
+}
 
-export const updateOne = model => async (req, res) => {}
+export const updateOne = model => async (req, res) => {
+  const doc = model.findByIdAndUpdate({
+    _id: req.params.id,
+     createdBy: req.user.id
+   },
+   req.body,
+   {new: true}
+ ).exec()
+ if(!doc) {
+   return res.status(400).end
+ }
+ res.status(200).json({data: doc})
+}
 
-export const removeOne = model => async (req, res) => {}
+export const removeOne = model => async (req, res) => {
+  const doc = model.findOneAndRemove({
+    _id: req.params.id,
+    createdBy: req.user._id
+  }).exec()
+  if (!doc) {
+    res.status(400)
+  }
+  res.status(200).json({data: doc})
+}
 
 export const crudControllers = model => ({
   removeOne: removeOne(model),
@@ -15,3 +53,11 @@ export const crudControllers = model => ({
   getOne: getOne(model),
   createOne: createOne(model)
 })
+
+/**
+ * GET / Read many
+ * GET /:id Read one
+ * POST / Create one
+ * PUT /:id Update one
+ * DELETE /:id Delete one
+**/
